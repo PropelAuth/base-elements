1c7305de4415cfdc586e8f6e0e7f105b1967491f
Josh <joshds313@gmail.com>
2023-01-04 15:04:03 -0500

    temp remove dependency on react lib

--- src/_types.ts
+++ src/_types.ts
@@ -1,23 +1,160 @@
-import {
-    AlertProps,
-    ButtonProps,
-    CheckboxProps,
-    ContainerProps,
-    DividerProps,
-    H1Props,
-    H3Props,
-    H5Props,
-    ImageProps,
-    InputProps,
-    LabelProps,
-    LinkProps,
-    ModalProps,
-    ParagraphProps,
-    PopoverProps,
-    ProgressProps,
-    SelectProps,
-    TableProps,
-} from "@propelauth/react"
+import { Dispatch, SetStateAction, CSSProperties, MouseEventHandler, ChangeEventHandler, ReactNode } from "react"
+
+type AlertProps = {
+    type?: "error" | "info" | "success" | "warning"
+    className?: string
+    style?: CSSProperties
+    children?: ReactNode
+}
+
+type ButtonProps = {
+    onClick?: MouseEventHandler<HTMLButtonElement>
+    loading?: boolean
+    disabled?: boolean
+    id?: string
+    className?: string
+    style?: CSSProperties
+    children?: ReactNode
+}
+
+type CheckboxProps = {
+    checked: boolean
+    onChange: ChangeEventHandler<HTMLInputElement>
+    id?: string
+    label?: ReactNode
+    required?: boolean
+    disabled?: boolean
+    className?: string
+    style?: CSSProperties
+}
+
+type ContainerProps = {
+    className?: string
+    style?: CSSProperties
+    children?: ReactNode
+}
+
+type DividerProps = {
+    className?: string
+    style?: CSSProperties
+    children?: ReactNode
+}
+
+type H1Props = {
+    className?: string
+    style?: CSSProperties
+    children?: ReactNode
+}
+
+type H3Props = {
+    className?: string
+    style?: CSSProperties
+    children?: ReactNode
+}
+
+type H5Props = {
+    className?: string
+    style?: CSSProperties
+    children?: ReactNode
+}
+
+type ImageProps = {
+    src: string
+    alt?: string
+    className?: string
+    style?: CSSProperties
+}
+
+type InputProps = {
+    value: string
+    onChange?: ChangeEventHandler<HTMLInputElement>
+    id?: string
+    type?: string
+    required?: boolean
+    disabled?: boolean
+    readOnly?: boolean
+    placeholder?: string
+    className?: string
+    style?: CSSProperties
+}
+
+type LabelProps = {
+    htmlFor?: string
+    className?: string
+    style?: CSSProperties
+    children?: ReactNode
+}
+
+type LinkProps = {
+    href: string
+    className?: string
+    style?: CSSProperties
+    children?: ReactNode
+}
+
+type ModalProps = {
+    show: boolean
+    setShow: Dispatch<SetStateAction<boolean>>
+    onClose?: () => void
+    className?: string
+    style?: CSSProperties
+    children?: ReactNode
+}
+
+type ParagraphProps = {
+    className?: string
+    style?: CSSProperties
+    children?: ReactNode
+}
+
+type PopoverProps = {
+    referenceElement: HTMLElement | null
+    show: boolean
+    setShow: Dispatch<SetStateAction<boolean>>
+    placement?: string
+    className?: string
+    style?: CSSProperties
+    children?: ReactNode
+}
+
+type ProgressProps = {
+    className?: string
+    style?: CSSProperties
+}
+
+type Option = {
+    label: string
+    value: string
+}
+
+type OptionGroup = {
+    label: string
+    options: Array<Option>
+}
+
+type Column = ReactNode
+
+type Row = {
+    [key: string]: ReactNode
+}
+
+type TableProps = {
+    columns: Column[]
+    rows?: Row[]
+    className?: string
+    style?: CSSProperties
+}
+
+type SelectProps = {
+    value: string
+    onChange: ChangeEventHandler<HTMLSelectElement>
+    options?: Array<OptionGroup | Option>
+    disabled?: boolean
+    id?: string
+    className?: string
+    style?: CSSProperties
+}
+
 export type BaseAlertProps = AlertProps
 export type BaseButtonProps = ButtonProps
 export type BaseCheckboxProps = CheckboxProps
